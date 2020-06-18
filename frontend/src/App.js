dca9f86c27b9527f7fdbf8089995c91f51102511
rushilp2311 <rushilp2311@gmail.com>
2020-06-18 19:55:31 -0400

    frontend added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,10 @@
 import React from "react";
 import "./App.css";
-import EmployeeList from "./EmployeeList";
+import "bootstrap/dist/css/bootstrap.min.css";
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      employees: [],
-    };
-  }
-
-  componentDidMount() {
-    fetch("/api/v1/employees")
-      .then((response) => response.text())
-      .then((employees) => {
-        this.setState({ employees: [employees] })
-      });
-  }
-  render() {
-    const employees = this.state;
-    return (
-      <div className="App">
-        <h1>{employees.map(employees => (
-
-        )}</h1>
-        <EmployeeList employees={e} />
-      </div>
-    );
-  }
+    render(){
+        
+    }
 }
 
 export default App;
