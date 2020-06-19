fafc780f38d8bc7fe3323c7c296d51e94de096ea
rushilp2311 <rushilp2311@gmail.com>
2020-06-19 17:20:49 -0400

    testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+const client = require("./client");
+
 class App extends React.Component {
-    render(){
-        
-    }
+  constructor(props) {
+    super(props);
+    this.state = { employees: [] };
+  }
+  componentDidMount() {
+    client({ method: "GET", path: "/api/employees" }).done((response) => {
+      this.setState({ employees: response.entity.employees });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.employees}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
--- springboot-postgresql-example/src/main/java/com/javaguide/springboot/model/ERole.java
+++
@@ -1,7 +0,0 @@
-package com.javaguide.springboot.model;
-
-public enum  ERole {
-    ROLE_USER,
-    ROLE_MODERATOR,
-    ROLE_ADMIN
-}
